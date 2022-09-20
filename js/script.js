8527223b80a6e9d844da1b01fa12289f1569b7c3
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-09-20 18:49:29 +0200

    add emails in HTML

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 console.log("Martedì");
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    raccolta: [],
+    rubrica: [],
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        //sostituendo RESPONSE con RISPOSTA non vedo più in HTML le mail...???
+        .then((response) => {
+          const singoloIndirizzo = response.data;
+          console.log(response.data);
+          this.raccolta.push(singoloIndirizzo.response);
+        });
+    }
+  },
+
+  // se aggiungo il secondo MOUNTED non viusualizzo più il secondo... ???
+
+  /*
+  mounted() {
+    for (i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/phone")
+        .then((response) => {
+          const numeroTelefono = response.data;
+          console.log(response.data);
+          this.rubrica.push(numeroTelefono.response);
+        });
+    }
+  },
+*/
+});
